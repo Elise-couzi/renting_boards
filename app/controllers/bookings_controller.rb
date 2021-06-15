dbe21b9b49c9fdbf885ea83c7890ea29a308a794
victorvanoverschelde <victorvanoverschelde@gmail.com>
2021-06-15 17:50:39 +0200

    ADD booking ok, view ok

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,20 +5,26 @@ class BookingsController < ApplicationController
 
   def new
     @board = Board.find(params[:board_id])
-    @user = current_user
     @booking = Booking.new
   end
 
-  def create #doit etre appelé par mon bouton submit de la page new
+  def create
     @user = current_user
     @board = Board.find(params[:board_id])
+    @booking = Booking.new(booking_params)
     @booking.user = @user
     @booking.board = @board
+
     if @booking.save
       redirect_to root_path
     else
       render :new
     end
   end
+  
+  private
 
+  def booking_params
+    params.require(:booking).permit(:start_at, :end_at)
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :board
+
+  validates :start_at, :end_at, presence: true
 end
