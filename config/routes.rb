1cfd04e78443ee687a59b0a5ba1e095e37da8e8b
vanessa tsang <va.tsang@yahoo.fr>
2021-06-15 11:42:14 +0200

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :boards do
+    resources :bookings, only: [:new, :create] #destroy?
+  end
+  get "/dashboard", to: "dashboard#show"
+  resources :bookings do
+    member do
+      patch "/validate", to: "bookings#validate"
+      patch "/decline", to: "bookings#decline"
+    end
+  end
 end
