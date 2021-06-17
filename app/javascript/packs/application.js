b81eabfc7218c0caa9b31f6168888c89f7c7ea49
elisecouzi <elisecouzi@hotmail.com>
2021-06-17 11:04:55 +0200

    fix turbolink map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,11 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import { initFlatpickr } from "../plugins/flatpickr";
 
-document.addEventListener('turbolinks:load', ()=>{
-initFlatpickr();
-});
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -32,13 +28,11 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-});
 
 import { initMapbox } from '../plugins/init_mapbox';
+import { initFlatpickr } from "../plugins/flatpickr";
 
 document.addEventListener('turbolinks:load', () => {
   initMapbox();
+  initFlatpickr();
 })
